180fdea334782798c5fb37d74a79497680bbb293
Aparnaraha <aparnaraha2912@gmail.com>
2024-10-07 22:43:00 +0530

    Lectures updated

--- Lecture-08/unary.cpp
+++ Lecture-08/unary.cpp
@@ -9,5 +9,12 @@ int main(){
 
     cout << a << endl; //Now, print 11 as value of a is increamented by +1 //Print 10 as it assign older value first
 
+    // post decreament and pre decreament
+    cout << "Pre Decreament ";
+    int b = 10;
+
+    cout << --b <<endl; //Print 9 as it is assign older value first
+
+    cout << b << endl; //Now, print 11 as value of a is increamented by +1 //Print 10 as it assign older value first
     
 }
