49d53b9e4f0d74ae6a39e94ddd4a19ca3a9f49d2
Aparnaraha <aparnaraha2912@gmail.com>
2024-11-21 23:05:30 +0530

    Lectures updated

---
+++ Lecture-14/selection_sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int arr[1000];
+  int n;
+  cout<<"Enter a number u want the array of : ";
+  cin>>n;
+
+  cout<<"Enter the element's in array : ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
