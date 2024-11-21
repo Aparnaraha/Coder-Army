87ff9844df58362e601ba46d19477057af116550
Aparnaraha <aparnaraha2912@gmail.com>
2024-11-21 23:05:38 +0530

    Lectures updated

--- Lecture-14/selection_sort.cpp
+++ Lecture-14/selection_sort.cpp
@@ -12,3 +12,20 @@ int main(){
     cin>>arr[i];
   }
 
+  // Selction sort  in ascending order 
+  for(int i=0;i<n-1;i++){
+    int index =i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j] < arr[index])
+      index =j;
+    }
+
+    swap(arr[i],arr[index]);
+  }
+
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+
+  return 0;
+}
