7db9853f72f3aceaef9eb0115dd5f5c2d4f3bcb8
Aparnaraha <aparnaraha2912@gmail.com>
2024-12-15 18:56:21 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
+++ Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
@@ -46,3 +46,11 @@ using namespace std;
     }
 
 
+int main(){
+  vector<int>arr = {2,3,3,3,3,4,5,6};
+  int target =3;
+  vector<int> ans = searchRange(arr, target);
+  for(auto it:ans){
+    cout<<it<<" ";
+  }
+}
