42cf5f79508497941ec3da1b496189ad52a3a3eb
Aparnaraha <aparnaraha2912@gmail.com>
2024-12-16 10:05:52 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
+++ Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
@@ -3,7 +3,47 @@
 using namespace std;
 
 
+ vector<int> searchRange(vector<int>& nums, int target) {
+       int start =0 , end =nums.size()-1 , mid ,  first =-1 ,last =-1;
 
+    //    for first occurance 
+    while(start <= end ){
+        mid = start +(end - start)/2;
+        if(nums[mid] == target){
+            first = mid;
+            end = mid -1;
+        }
+        else if(nums[mid] < target){
+            start = mid+1;
+        }
+        else{
+            end =mid -1;
+        }
+    }
+
+     //    for last occurance 
+
+     start =0, end =nums.size()-1;
+    while(start <= end ){
+        mid = start +(end - start)/2;
+        if(nums[mid] == target){
+            last = mid;
+            start = mid + 1;
+        }
+        else if(nums[mid] < target){
+            start = mid+1;
+        }
+        else{
+            end =mid -1;
+        }
+    }
+
+    vector<int> a(2);
+    a[0] =first;
+    a[1] = last;
+
+    return a;
+    }
 
 
 int main(){
