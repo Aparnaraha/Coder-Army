f83f37a2a598e0242e1aae5dd38742ca1b17d2b1
Aparnaraha <aparnaraha2912@gmail.com>
2024-12-20 21:52:48 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
+++ Lecture-15/Problems_On_Binary_Search/first_last_Occur.cpp
@@ -21,22 +21,7 @@ using namespace std;
         }
     }
 
-     //    for last occurance 
 
-     start =0, end =nums.size()-1;
-    while(start <= end ){
-        mid = start +(end - start)/2;
-        if(nums[mid] == target){
-            last = mid;
-            start = mid + 1;
-        }
-        else if(nums[mid] < target){
-            start = mid+1;
-        }
-        else{
-            end =mid -1;
-        }
-    }
 
     vector<int> a(2);
     a[0] =first;
