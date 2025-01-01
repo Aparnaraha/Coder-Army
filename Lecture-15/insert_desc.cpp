04e6354afd82ea11b63af7dc05f65eb54c9fd42c
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-01 12:16:05 +0530

    Lectures updated

--- Lecture-15/insert_desc.cpp
+++ Lecture-15/insert_desc.cpp
@@ -11,7 +11,19 @@ int main(){
     cin>>arr[i];
   }
 
- 
+  // Intersion sort logic 
+  for(int i=1;i<n;i++){
+    for(int j =i;j>0;j--){
+       if(arr[j] > arr[j-1]){
+        // swapping the arr[j] and arr[j-1] variable 
+        int temp = arr[j];
+        arr[j] =arr[j-1];
+        arr[j-1] = temp;
+       }
+       else 
+       break;
+    }
+  }
 
   for(int i=0;i<n;i++){
     cout<<arr[i]<<" ";
