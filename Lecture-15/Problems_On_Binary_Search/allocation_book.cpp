f50f908f01df8476b4601b100395737b1101a464
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-04 00:04:30 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/allocation_book.cpp
+++ Lecture-15/Problems_On_Binary_Search/allocation_book.cpp
@@ -1,18 +1,7 @@
  #include<iostream>
  using namespace std;
  
- int findPages(int A[], int N, int M) 
-    {
-        if(M > N) return -1;
-        
-        int start =0 ,end =0,mid , ans;
-        for(int i=0;i<N;i++)
-        {
-           start =max(start,A[i]);
-           end +=A[i];
-        }  
-        
-        
+ 
         while(start <= end){
             mid = start + (end-start)/2;
             
