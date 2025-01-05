d6b29f44dd4a6a695795cfc25bc2283469ef7f4f
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-05 23:17:15 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/allocation_book.cpp
+++ Lecture-15/Problems_On_Binary_Search/allocation_book.cpp
@@ -13,26 +13,6 @@
         }  
         
         
-        while(start <= end){
-            mid = start + (end-start)/2;
-            
-            int pages =0 , count =1;
-            for(int i=0;i<N;i++){
-                pages +=A[i];
-                if(pages > mid){
-                    count++;
-                    pages =A[i];
-                }
-            }
-            if(count <= M){
-                ans =mid;
-                end =mid-1;
-            }
-            else{
-                start =mid+1;
-            }
-        }
-        
-        return ans;
+  
         
     }
