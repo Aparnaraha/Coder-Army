6acb3bb191bdc1d4f73c86a4b5f62170ce27ae75
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-15 01:20:51 +0530

    Lectures updated

--- Lecture-15/binary_search.cpp
+++ Lecture-15/binary_search.cpp
@@ -11,22 +11,3 @@ int BinarySearch(int arr[],int n,int key){
   }
   return -1;
 }
-
-int main(){
-  int arr[1000];
-  int n;
-  cout<<"Enter the number u want the array of :";
-  cin>>n;
-
-cout<<"Enter the elements of the array";
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-  }
-
-  int key;
-  cout<<"Enter the key u want to search in array :";
-  cin>>key;
-
-  int ans = BinarySearch(arr,n,key);
-  cout<<ans;
-}
