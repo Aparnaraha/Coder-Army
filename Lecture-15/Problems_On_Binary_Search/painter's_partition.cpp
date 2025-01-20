728f802a7a8e08eb4074384be16a5efc79e5b594
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-20 22:26:09 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/painter's_partition.cpp
+++ Lecture-15/Problems_On_Binary_Search/painter's_partition.cpp
@@ -12,7 +12,25 @@ long long minTime(int A[], int N, int M)
         }  
         
         
-       
+        while(start <= end){
+            mid = start + (end-start)/2;
+            
+            long long pages =0 , count =1;
+            for(int i=0;i<N;i++){
+                pages +=A[i];
+                if(pages > mid){
+                    count++;
+                    pages =A[i];
+                }
+            }
+            if(count <= M){
+                ans =mid;
+                end =mid-1;
+            }
+            else{
+                start =mid+1;
+            }
+        }
         
         return ans;
     }
