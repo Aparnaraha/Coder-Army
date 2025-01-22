d14c329e518237fc73e3a7217c0b3ff54f1b4163
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-22 23:08:20 +0530

    Lectures updated

---
+++ Lecture-15/Problems_On_Binary_Search/peak_element.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+  
+  return -1;
+}
+
+int main()
+{
+
+  vector<int> arr = {1, 6, 3, 2, 1};
+  int ans = peakIndexInMountainArray(arr);
+
+  cout << ans << " ";
+}
