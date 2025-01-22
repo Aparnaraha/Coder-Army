fc35a09035cafe391e234d2ed45046c092b56c12
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-22 23:08:31 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/peak_element.cpp
+++ Lecture-15/Problems_On_Binary_Search/peak_element.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int peakIndexInMountainArray(vector<int> &arr)
 {
-  
+  int start = 0, end = arr.size() - 1, mid;
+
+  while (start <= end)
+  {
+    mid = end + (start - end) / 2;
+    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+    {
+      return mid;
+    }
+    else if (arr[mid] > arr[mid - 1])
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+  }
   return -1;
 }
 
