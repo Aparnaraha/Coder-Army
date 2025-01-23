1671057c2ee386a0f75015a21ec5314d14877984
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-23 10:55:15 +0530

    Lectures updated

---
+++ Lecture-15/Problems_On_Binary_Search/search_insert.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+    int main(){
+  vector<int>arr = {1,4,6,7,8,9};
+  int target =5;
+int ans = searchInsert(arr, target);
+  
+    cout<<ans<<" ";
+ 
+}
