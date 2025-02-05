64a98a4e9d83646be44e187bf25c355083145c72
Aparnaraha <aparnaraha2912@gmail.com>
2025-02-05 23:47:06 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/search_insert.cpp
+++ Lecture-15/Problems_On_Binary_Search/search_insert.cpp
@@ -3,26 +3,7 @@
 #include<vector>
 using namespace std;
 
-int searchInsert(vector<int>& arr, int target) {
-       int start =0 , end = arr.size()-1 ,ans =arr.size();
 
-       while(start <= end ){
-           int mid = start + (end - start)/2;
-
-           if(arr[mid] == target){
-               ans = mid ;
-               return ans;
-           }
-           else if(arr[mid] < target){
-               start =mid+1;
-           }
-           else{
-               ans = mid;
-               end = mid-1;
-           }
-       }
-       return ans;
-    }
 
     int main(){
   vector<int>arr = {1,4,6,7,8,9};
