cba84e9cbac3e266ef76ecbee4958ed28eaa339d
Aparnaraha <aparnaraha2912@gmail.com>
2025-02-15 23:53:27 +0530

    Lectures updated

--- Lecture-15/binary_search.cpp
+++ Lecture-15/binary_search.cpp
@@ -2,14 +2,7 @@
 using namespace std;
 
 int BinarySearch(int arr[],int n,int key){
-  int start =0 , end =n-1 , mid;
-  while(start <= end){
-    mid = start + (end -start)/2;
-    if(arr[mid] == key) return mid;
-    else if(arr[mid] < key ) start =mid+1;
-    else end  =mid -1;
-  }
-  return -1;
+  
 }
 
 int main(){
