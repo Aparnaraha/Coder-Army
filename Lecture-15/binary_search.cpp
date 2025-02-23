662322450554a7982ea47304bfafcf77fa45e053
Aparnaraha <aparnaraha2912@gmail.com>
2025-02-23 23:29:05 +0530

    Lectures updated

--- Lecture-15/binary_search.cpp
+++ Lecture-15/binary_search.cpp
@@ -1,32 +1,20 @@
-#include<iostream>
+#include <iostream> 
+#include<vector>
 using namespace std;
 
-int BinarySearch(int arr[],int n,int key){
-  int start =0 , end =n-1 , mid;
-  while(start <= end){
-    mid = start + (end -start)/2;
-    if(arr[mid] == key) return mid;
-    else if(arr[mid] < key ) start =mid+1;
-    else end  =mid -1;
-  }
-  return -1;
-}
-
-int main(){
-  int arr[1000];
-  int n;
-  cout<<"Enter the number u want the array of :";
-  cin>>n;
-
-cout<<"Enter the elements of the array";
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-  }
-
-  int key;
-  cout<<"Enter the key u want to search in array :";
-  cin>>key;
-
-  int ans = BinarySearch(arr,n,key);
-  cout<<ans;
+// tc : o(log(n*m))  , sc: o(1)
+bool findInMatrix(int target, vector<vector<int>> &mat)
+{
+    int n =mat.size();
+         int m =mat[0].size();
+          int low =0, high =n*m -1;
+    while(low <= high){
+        int mid =(low +high)/2;
+        int row = mid/m;
+        int col = mid%m;
+        if(mat[row][col] == target) return true;
+        else if(mat[row][col] < target) low =mid+1;
+        else high = mid -1;
+    }
+    return false;
 }
