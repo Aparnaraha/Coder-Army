094588d4bf8444aefd05a66fd0edea73fa9445bc
Aparnaraha <aparnaraha2912@gmail.com>
2025-03-16 23:32:46 +0530

    Lectures updated

--- Lecture-15/Problems_On_Binary_Search/squart.cpp
+++ Lecture-15/Problems_On_Binary_Search/squart.cpp
@@ -1,32 +1,40 @@
- #include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
- 
- int mySqrt(int x) {
-       if(x <= 1) return x;
-       int left =1;
-       int right =x;
-       int ans =0;
-       while(left <= right){
-           int mid = left + (right -left)/2;
-           if (mid == x / mid) {
-                return mid;
-            } else if (mid < x / mid) {
-                left = mid + 1;
-                ans = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
 
-        return ans;
+int mySqrt(int x)
+{
+    if (x <= 1)
+        return x;
+    int left = 1;
+    int right = x;
+    int ans = 0;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (mid == x / mid)
+        {
+            return mid;
+        }
+        else if (mid < x / mid)
+        {
+            left = mid + 1;
+            ans = mid;
+        }
+        else
+        {
+            right = mid - 1;
+        }
     }
 
-      int main(){
-  
- int n =80;
-int ans = mySqrt(n);
-  
-    cout<<ans<<" ";
- 
+    return ans;
+}
+
+int main()
+{
+
+    int n = 80;
+    int ans = mySqrt(n);
+
+    cout << ans << " ";
 }
