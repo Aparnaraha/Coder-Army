0aaa80134924ba003b749b0b878088023ac2170b
Aparnaraha <aparnaraha2912@gmail.com>
2025-03-21 23:25:40 +0530

    Lectures updated

--- Lecture-15/Tough_Problems/agressive_cows.cpp
+++ Lecture-15/Tough_Problems/agressive_cows.cpp
@@ -1,20 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
 
 
-
-Algorithm LCS(X, Y):
-    Input: Strings X of length n, Y of length m
-    Output: Length of the Longest Common Subsequence of X and Y
-
-    // Step 1: Create a DP table of size (n+1) x (m+1)
-    Initialize a 2D array dp of size (n+1) x (m+1)
-
-    // Step 2: Fill the DP table using the recurrence relation
-    for i = 1 to n:
-        for j = 1 to m:
-            if X[i-1] == Y[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-    // Step 3: The length of the LCS is in dp[n][m]
-    return dp[n][m]
+ int solve(int n, int k, vector<int> &stalls) {
+    
+        int start =1 ,end ,mid ,ans;
+        sort(stalls.begin(),stalls.end());
+        end = stalls[n-1] - stalls[0];
+        
+        while(start <= end ){
+            mid = start + (end -start)/2;
+            
+            int count =1 ,pos =stalls[0];
+            for(int i=1;i<n;i++){
+                if(pos + mid <= stalls[i])
+                {
+                    count++;
+                    pos =stalls[i];
+                }
+            }
+            
+            if(count < k)
+            {
+                end = mid -1;
+            }
+            else{
+                ans = mid;
+                start =mid+1;
+            }
+        }
+        return ans;
+    }
